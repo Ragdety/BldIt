7c8b8943b5333f2963f2861fed37e2f77872f3a8
Edgar Terrazas <ragdeterra@gmail.com>
2022-08-07 18:54:01 -0500

    Re-setup react project with tailwind

--- bldit.web/src/App.tsx
+++ bldit.web/src/App.tsx
@@ -5,7 +5,7 @@ import {HubConnection} from "@microsoft/signalr";
 
 function App() {
     const [connection, setConnection] = useState<HubConnection | null>();
-    
+
     useEffect(() => {
         const connection = new signalR.HubConnectionBuilder()
             .withUrl('https://localhost:5001/buildStream', {
@@ -17,7 +17,7 @@ function App() {
 
         setConnection(connection);
     }, [])
-    
+
     useEffect(() => {
         if(connection) {
             connection.start()
@@ -30,16 +30,25 @@ function App() {
                 .catch((e: any) => console.log('Connection failed: ', e));
         }
     }, [connection])
-    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <nav>Nav here</nav>
-        <main>Main here</main>
-        <footer>Footer here</footer>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <nav>Nav here</nav>
+                <main>
+                    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+                        <p className="text-3xl text-gray-700 font-bold mb-5">
+                            Welcome!
+                        </p>
+                        <p className="text-gray-500 text-lg">
+                            React and Tailwind CSS in action
+                        </p>
+                    </div>
+                </main>
+                <footer>Footer here</footer>
+            </header>
+        </div>
+    );
 }
 
 export default App;
