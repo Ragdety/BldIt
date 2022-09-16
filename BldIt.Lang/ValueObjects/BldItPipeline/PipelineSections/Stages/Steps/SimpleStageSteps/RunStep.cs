84a2a1c0253828cf31da2802ad9adf0a1717caf2
Edgar Terrazas <ragdeterra@gmail.com>
2022-09-16 00:52:48 -0500

    Started adding support for run step to run processes, tested with a sample batch script

--- BldIt.Lang/ValueObjects/BldItPipeline/PipelineSections/Stages/Steps/SimpleStageSteps/RunStep.cs
+++ BldIt.Lang/ValueObjects/BldItPipeline/PipelineSections/Stages/Steps/SimpleStageSteps/RunStep.cs
@@ -3,8 +3,8 @@
 public class RunStep : SimpleStageStep
 {
     public string Command { get; }
-    public string Arguments { get; }
-    public string WorkingDirectory { get; }
+    public string? Arguments { get; }
+    public string? WorkingDirectory { get; }
     public int ErrorCode { get; set; }
     public override string StepIdentifier { get; protected init; } 
         = nameof(RunStep).Replace("Step", "").ToLower();
@@ -12,15 +12,15 @@ public class RunStep : SimpleStageStep
     private RunStep(
         SimpleStepType stepType,
         string command,
-        string arguments,
-        string workingDirectory) : base(stepType)
+        string? arguments,
+        string? workingDirectory) : base(stepType)
     {
         Command = command;
         Arguments = arguments;
         WorkingDirectory = workingDirectory;
     }
     
-    public RunStep(string command, string arguments, string workingDirectory) 
+    public RunStep(string command, string? arguments, string? workingDirectory) 
         : this(SimpleStepType.RunStep, command, arguments, workingDirectory)
     {
         Command = command;
--- BldIt.Lang/Visitors/PipelineVisitors/PipelineSections/Stages/Steps/SimpleStepStatementVisitor.cs
+++ BldIt.Lang/Visitors/PipelineVisitors/PipelineSections/Stages/Steps/SimpleStepStatementVisitor.cs
@@ -1,4 +1,5 @@
-﻿using BldIt.Lang.Exceptions;
+﻿using BldIt.Api.Services.Processes;
+using BldIt.Lang.Exceptions;
 using BldIt.Lang.Grammar;
 using BldIt.Lang.ValueObjects.BldItExpressions;
 using BldIt.Lang.ValueObjects.BldItExpressions.ConstantTypes;
@@ -37,7 +38,7 @@ public class SimpleStepStatementVisitor : StepStatementVisitor
         return stepIdentifier switch
         {
             "echo" => VisitEchoStep(context),
-            //"run" => VisitRunStep(context),
+            "run" => VisitRunStep(context),
             "error" => VisitErrorStep(context),
             _ => throw new CompilingException($"Step {stepIdentifier} is not supported")
         };
@@ -73,5 +74,88 @@ public class SimpleStepStatementVisitor : StepStatementVisitor
         Console.Error.WriteLine(exprTypeValueObject.ToString());
         
         return new ErrorStep(exprTypeValueObject.ToString());
+    }
+    
+    private RunStep VisitRunStep(BldItParser.PipelineSimpleStepCallContext context)
+    {
+        /*
+         * 1) Check arguments length
+         * 2) Evaluate arguments (as stringValues)
+         * 3) Check if expression evaluation is of type StringValue
+         * 4) Get the actual Value of the StringValue
+         * 5) Run new Process here using the argument values
+         * 6) Return the result of the process execution into RunStep class
+         */
+        
+        var stepExpressions = context.pipelineExpression();
+
+        switch (stepExpressions.Length)
+        {
+            case < 1:
+                throw new CompilingException("Step \'run\' requires at least 1 argument: run(command/path)");
+            case > 3:
+                throw new CompilingException("Step \'run\' requires at most 3 arguments (command, arguments, working directory)");
+        }
+
+        var pipelineExpressionVisitor = new PipelineExpressionVisitor(SemanticErrors, GlobalVariables, Functions, GlobalEnv, Parameters);
+        var command = pipelineExpressionVisitor.VisitPipelineExpression(stepExpressions[0]);
+        
+        var arguments =
+            stepExpressions.Length > 1 ? 
+                pipelineExpressionVisitor.VisitPipelineExpression(stepExpressions[2]) 
+                : new StringValue("");
+        
+        //If run step is called without working directory, use current directory
+        //Working directory is the 3rd argument like this: run("command", "arguments", "working directory")
+        var workingDirectory =
+            stepExpressions.Length > 2 ? 
+                pipelineExpressionVisitor.VisitPipelineExpression(stepExpressions[3]) 
+                : new StringValue("");
+        
+        CheckRunStepExpressionTypes(command, arguments, workingDirectory);
+
+        var commandValue = ExpressionTypeHelper.GetValueFromType(command);
+        var argumentsValue = ExpressionTypeHelper.GetValueFromType(arguments);
+        var workingDirectoryValue = ExpressionTypeHelper.GetValueFromType(workingDirectory);
+
+        var runStep = new RunStep(
+            commandValue.ToString() 
+            ?? throw new CompilingException("Command value is null"),
+            argumentsValue.ToString(), 
+            workingDirectoryValue.ToString());
+        
+        //5) Run process here
+        var launcherService = new LauncherService
+        {
+            ProgramFileName = runStep.Command,
+        };
+        
+        if (runStep.Arguments is not null)
+            launcherService.Arguments = runStep.Arguments;
+        if(runStep.WorkingDirectory is not null)
+            launcherService.WorkingDirectory = runStep.WorkingDirectory;
+
+        //Sends process output to frontend
+        async void OutputHandler(string output)
+        {
+            //await _hub.Clients.All.SendAsync("OutputReceived", output, cancellationToken);
+            //TODO: We should send the output to the frontend here.
+            //TODO: Or connect the output stream of the main BldIt lang process to the frontend
+            await Console.Out.WriteLineAsync(output);
+        }
+        
+        var exitCode = launcherService.Run(OutputHandler);
+        runStep.ErrorCode = exitCode;
+        return runStep;
+    }
+
+    private static void CheckRunStepExpressionTypes(Expression command, Expression arguments, Expression workingDir)
+    {
+        if (command is not StringValue)
+            throw new InvalidDataTypeException("Step \'run\' requires a string as the first argument (command)");
+        if (arguments is not StringValue)
+            throw new InvalidDataTypeException("Step \'run\' requires a string of command arguments as the second argument");
+        if (workingDir is not StringValue)
+            throw new InvalidDataTypeException("Step \'run\' requires a string as the third argument (working directory)");
     }
 }
