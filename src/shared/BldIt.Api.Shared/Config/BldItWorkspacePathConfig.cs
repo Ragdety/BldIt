b1c3dd4c530da1f0670f329f21cae62336ed9d7e
Edgar Terrazas <ragdeterra@gmail.com>
2022-11-14 23:22:06 -0500

    Added Path bldit config class

---
+++ src/shared/BldIt.Api.Shared/Config/BldItWorkspacePathConfig.cs
@@ -0,0 +1,34 @@
+﻿using BldIt.Api.Shared.Settings;
+using Microsoft.Extensions.Options;
+
+namespace BldIt.Api.Shared.Config;
+
+public class BldItWorkspacePathConfig
+{
+    private const string ProjectsFolderName = "Projects";
+    private const string JobsFolderName = "Jobs";
+    private const string TempFolderName = "Temp";
+    private const string ProjectNamePrefix = "{projectName}";
+
+    public BldItWorkspacePathConfig()
+    {
+        var bldItHome = Environment.GetEnvironmentVariable(BldItApiConstants.BldItEnvironmentNames.BLDIT_HOME);
+        BldItHome = bldItHome ?? throw new ArgumentNullException(nameof(bldItHome));
+    }
+    
+    public BldItWorkspacePathConfig(IOptionsMonitor<BldItWorkspacePathSettings> options)
+    {
+        var settings = options.CurrentValue;
+        BldItHome = settings.BldItHome;
+    }
+
+    public string BldItHome { get; }
+    
+    public string ProjectsPath => Path.Combine(BldItHome, ProjectsFolderName);
+    
+    public string JobsPath => Path.Combine(ProjectsPath, ProjectNamePrefix, JobsFolderName);
+    
+    public string JobsPathForProject(string projectName) => JobsPath.Replace(ProjectNamePrefix, projectName);
+    
+    public string TempPath => Path.Combine(BldItHome, TempFolderName);
+}
---
+++ src/shared/BldIt.Api.Shared/Settings/BldItWorkspacePathSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BldIt.Api.Shared.Settings;
+
+public class BldItWorkspacePathSettings
+{
+    /// <summary>
+    /// Represents the home where the bldIt hosting installation is located
+    /// </summary>
+    public string BldItHome { get; set; }
+}
