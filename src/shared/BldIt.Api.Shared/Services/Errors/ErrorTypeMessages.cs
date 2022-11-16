153f7321a9f62ede71495fe2895a3dd033cada01
Edgar Terrazas <ragdeterra@gmail.com>
2022-11-16 00:44:21 -0500

    Added common error messages types

---
+++ src/shared/BldIt.Api.Shared/Services/Errors/ErrorTypeMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace BldIt.Api.Shared.Services.Errors;
+
+public static class ErrorTypeMessages
+{
+    public const string ExistingInstance = "Instance already exists";
+    public const string InvalidInstance  = "Invalid instance";
+    public const string InstanceNotFound = "Instance was not found";
+    public const string InstanceNotOwned = "Instance does not belong to current user";
+}
