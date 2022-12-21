22f6460ec264506c51e2e3589f9e52b4c14f6f4e
Edgar Terrazas <ragdeterra@gmail.com>
2022-12-20 23:49:54 -0500

    Updated contracts for DeletedBuilds

---
+++ src/services/builds/BldIt.Builds.Contracts/Contracts/JobBuildsDeleted.cs
@@ -0,0 +1,3 @@
+﻿namespace BldIt.Builds.Contracts.Contracts;
+
+public record JobBuildsDeleted(Guid JobId);
---
+++ src/services/jobs/BldIt.Jobs.Contracts/Contracts/JobUpdated.cs
@@ -0,0 +1,6 @@
+﻿namespace BldIt.Jobs.Contracts.Contracts;
+
+public record JobUpdated(
+    Guid JobId,
+    string JobName
+    );
