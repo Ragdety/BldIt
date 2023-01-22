cbdccca8d3c006d517763224d7547b7f865f6b76
Edgar Terrazas <ragdeterra@gmail.com>
2023-01-22 01:37:57 -0500

    Added settings for workers

---
+++ src/shared/BldIt.Api.Shared/Settings/BldItWorkerSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BldIt.Api.Shared.Settings;
+
+public class BldItWorkerSettings
+{
+    /// <summary>
+    /// Number of workers available in the system
+    /// </summary>
+    public int WorkerNumber { get; set; }
+}
