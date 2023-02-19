7151042c06e8628e77871267a59ef8c327e85839
Edgar Terrazas <ragdeterra@gmail.com>
2023-02-18 23:08:58 -0500

    Added token lifetime to jwt settings

--- src/shared/BldIt.Api.Shared/Settings/JwtSettings.cs
+++ src/shared/BldIt.Api.Shared/Settings/JwtSettings.cs
@@ -1,6 +1,7 @@
-﻿namespace BldIt.Identity.Core.Settings;
+﻿namespace BldIt.Api.Shared.Settings;
 
 public class JwtSettings
 {
     public string Secret { get; set; }
+    public TimeSpan TokenLifetime { get; set; }
 }
